75f26047d4b874ad5ef7fb9cc03d2c7cd0ade358
k.sakamoto <sakamoto.tis2021@gamil.com>
2021-06-11 10:43:34 +0900

    カテゴリのドロップダウンリストに不備があったので再度プルをお願いします。

--- shared_shop/src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
@@ -5,7 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.shop.entity.Category;
+import jp.co.sss.shop.repository.CategoryRepository;
 import jp.co.sss.shop.repository.ItemRepository;
 
 /**
@@ -20,6 +23,9 @@ public class ItemShowCustomerController {
 	 */
 	@Autowired
 	ItemRepository itemRepository;
+
+	@Autowired
+	CategoryRepository categoryRepository;
 
 	/**
 	 * トップ画面 表示処理
@@ -37,6 +43,25 @@ public class ItemShowCustomerController {
 	@RequestMapping("/newItems")
 	public String showNewItemList(Model model) {
 		return "item/list/item_list";
+	}
+
+	@RequestMapping(path = "/categorySearch", method = RequestMethod.POST)
+	public String categorySearch(Integer id, Model model) {
+		Category category = new Category();
+
+		category.setId(id);
+
+		model.addAttribute("genre", categoryRepository.findAll());
+		model.addAttribute("items", itemRepository.findByCategory(categoryRepository.getOne(id)));
+		return "item/list/item_list";
+	}
+
+	@RequestMapping(path = "/itemSearch", method = RequestMethod.POST)
+	public String itemSearch(String name, Model model) {
+		model.addAttribute("items",itemRepository.findByNameLike("%" + name + "%"));
+
+		return "item/list/itemSearch";
+
 	}
 
 }
